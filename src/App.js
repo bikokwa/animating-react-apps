e02730ecb2d2f52bd9331f8183a1eff8e5865fa9
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-07-24 13:42:02 +0300

    using the transition component

--- src/App.js
+++ src/App.js
@@ -51,13 +51,16 @@ class App extends Component {
             />
           )}
         </Transition>
+        <Transition
+          in={this.state.modalIsOpen}
+          timeout={300}
+          mountOnEnter
+          unmountOnExit
+        >
+          {(state) => <Modal show={state} closed={this.closeModal} />}
+        </Transition>
         <br />
-        {this.state.modalIsOpen ? (
-          <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
-        ) : null}
-        {this.state.modalIsOpen ? (
-          <Backdrop show={this.state.modalIsOpen} />
-        ) : null}
+        {this.state.modalIsOpen ? <Backdrop show /> : null}
         <button className="Button" onClick={this.showModal}>
           Open Modal
         </button>
